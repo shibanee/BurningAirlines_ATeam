7eb4a7290e2a4cfc1df7c19648695af89b317d5d
James MacLeod <james@macdoch.com>
2018-02-06 16:34:12 +1100

    commiting search template

---
+++ react-frontend/fakeServerData.js
@@ -0,0 +1,39 @@
+let fakeServerData = {
+  flights: [
+    {
+      flight_no: 1234,
+      date: "12/01/2018",
+      origin: "SYD",
+      destination: "MEL"
+    },
+    {
+      flight_no: 3478,
+      date: "13/01/2018",
+      origin: "HOB",
+      destination: "MEL"
+    },
+    {
+      flight_no: 1234,
+      date: "12/01/2018",
+      origin: "SYD",
+      destination: "HOB"
+    }
+  ],
+  airplanes: [
+    {
+      name: "Virgin Airlines",
+      rows: "20",
+      columns: "6"
+    },
+    {
+      name: "Qantas",
+      rows: "20",
+      columns: "6"
+    },
+    {
+      name: "Emirates",
+      rows: "20",
+      columns: "6"
+    }
+  ]
+};
--- react-frontend/src/components/SearchFlights.js
+++ react-frontend/src/components/SearchFlights.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 
 class SearchFlights extends Component {
+  constructor() {
+    super();
+    this.state = { query: "" };
+    this._handleInput = this._handleInput.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+  }
+
+  _handleInput(e) {
+    this.setState({ query: e.target.value });
+  }
+
   render() {
     return (
       <div>
-        <input type="text" />
-        <button type="sumbit">Search Flights</button>
+        <form onSubmit={this._handleSubmit}>
+          <input type="text" onInput={this._handleInput} />
+          <button type="submit">Search Flights</button>
+        </form>
       </div>
     );
   }
